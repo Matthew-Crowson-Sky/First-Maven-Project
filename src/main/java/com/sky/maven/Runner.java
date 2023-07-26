c08d72b214767cc1a103586cbed61eafc04222b1
Matthew-Crowson-Sky <matthew.crowson@sky.uk>
2023-07-26 11:01:13 +0100

    updated project to include dependency

--- src/main/java/com/sky/maven/Runner.java
+++ src/main/java/com/sky/maven/Runner.java
@@ -1,7 +1,11 @@
 package com.sky.maven;
+import com.sky.demo.Calculator;
 
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello Maven.");
+
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(5, 2));
     }
 }
